33798b14dc2a622ee9bf0457d5581a80089a40ac
ArunLoga <97372301+ArunLoga@users.noreply.github.com>
2022-01-28 14:13:45 +0800

    Removed unused assets

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,115 +4,47 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-	public float speed = 5;
-	public float waitTime = .3f;
-	public float turnSpeed = 90;
+    /*private float speed = 7.0f;
+    private float horInput;
+    private float verInput;
+    private float turnspeed = 30.0f;
 
-	public Light spotlight;
-	public float viewDistance;
-	public LayerMask viewMask;
-	float viewAngle;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }*/
 
-	public Transform pathHolder;
-	Transform player;
-	Color originalSpotlightColour;
+    public float moveSpeed = 7;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8;
 
-	void Start()
-	{
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-		viewAngle = spotlight.spotAngle;
-		originalSpotlightColour = spotlight.color;
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
 
-		Vector3[] waypoints = new Vector3[pathHolder.childCount];
-		for (int i = 0; i < waypoints.Length; i++)
-		{
-			waypoints[i] = pathHolder.GetChild(i).position;
-			waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
-		}
+    new Rigidbody rigidbody;
 
-		StartCoroutine(FollowPath(waypoints));
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
-	}
+    void Update()
+    {
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
 
-	void Update()
-	{
-		if (CanSeePlayer())
-		{
-			spotlight.color = Color.red;
-		}
-		else
-		{
-			spotlight.color = originalSpotlightColour;
-		}
-	}
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
 
-	bool CanSeePlayer()
-	{
-		if (Vector3.Distance(transform.position, player.position) < viewDistance)
-		{
-			Vector3 dirToPlayer = (player.position - transform.position).normalized;
-			float angleBetweenGuardAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
-			if (angleBetweenGuardAndPlayer < viewAngle / 2f)
-			{
-				if (!Physics.Linecast(transform.position, player.position, viewMask))
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
 
-	IEnumerator FollowPath(Vector3[] waypoints)
-	{
-		transform.position = waypoints[0];
-
-		int targetWaypointIndex = 1;
-		Vector3 targetWaypoint = waypoints[targetWaypointIndex];
-		transform.LookAt(targetWaypoint);
-
-		while (true)
-		{
-			transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
-			if (transform.position == targetWaypoint)
-			{
-				targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
-				targetWaypoint = waypoints[targetWaypointIndex];
-				yield return new WaitForSeconds(waitTime);
-				yield return StartCoroutine(TurnToFace(targetWaypoint));
-			}
-			yield return null;
-		}
-	}
-
-	IEnumerator TurnToFace(Vector3 lookTarget)
-	{
-		Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
-		float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
-
-		while (Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle) > 0.05f)
-		{
-			float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
-			transform.eulerAngles = Vector3.up * angle;
-			yield return null;
-		}
-	}
-
-	void OnDrawGizmos()
-	{
-		Vector3 startPosition = pathHolder.GetChild(0).position;
-		Vector3 previousPosition = startPosition;
-
-		foreach (Transform waypoint in pathHolder)
-		{
-			Gizmos.DrawSphere(waypoint.position, .3f);
-			Gizmos.DrawLine(previousPosition, waypoint.position);
-			previousPosition = waypoint.position;
-		}
-		Gizmos.DrawLine(previousPosition, startPosition);
-
-		Gizmos.color = Color.red;
-		Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
-	}
-
+    void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+    }
 }
